4ab066a45d483768ebaa5082b3bc4c68ae10e572
Kim Jiae <95462028+gimziae@users.noreply.github.com>
2022-12-08 21:33:49 +0900

    11

--- controllers/mysqlConnect.js
+++ controllers/mysqlConnect.js
@@ -1,15 +1,15 @@
 // @ts-check
 
-const mysql = require('mysql');
+// const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'ji78ji78',
-  port: 3306, // mysql 공식 포트번호
-  database: 'overall',
-});
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'ji78ji78',
+//   port: 3306, // mysql 공식 포트번호
+//   database: 'overall',
+// });
 
-connection.connect();
+// connection.connect();
 
-module.exports = connection;
+// module.exports = connection;
